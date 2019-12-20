c2ac15b626f7973540626c8a898f8ea848d62b3d
patilup@mfcgd.com <patilup@prd.manulifeusa.com>
2019-12-20 12:01:45 -0500

    inisvc - build & dockerize

---
+++ tasks.backend.initservice/Controllers/InitialController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using tasks.backend.initservice.Models;
+using tasks.backend.initservice.Services;
+
+namespace tasks.backend.initservice.Controllers
+{
+    [ApiController]
+    [Route("initapi/v1/[controller]")]
+    public class InitialController : ControllerBase
+    {
+        private readonly ILogger<InitialController> _logger;
+        private readonly IExchangeRate _exchangeRate;
+        private readonly IPublishEventService _publishEventService;
+        private string eventLog;
+        public InitialController(ILogger<InitialController> logger
+            , IExchangeRate exchangeRate
+            , IPublishEventService publishEventService)
+        {
+            _logger = logger;
+            _exchangeRate = exchangeRate;
+            _publishEventService = publishEventService;
+        }
+
+        [HttpGet]
+        [Route("hpu")]
+        public async Task<IActionResult> Get()
+        {
+            eventLog = "api get call --" + System.Guid.NewGuid().ToString() + " : " + System.DateTimeOffset.Now.ToString();
+            Console.WriteLine(eventLog);
+            string jsonString = "{name:Tom Riddle, title:The Dark Lord}";
+            //return Ok(await _taskService.GetTasks("Kingslayer"));
+            eventLog = "api get call ++" + System.Guid.NewGuid().ToString() + " : " + System.DateTimeOffset.Now.ToString();
+            Console.WriteLine(eventLog);
+            return Ok(jsonString);
+        }
+        [HttpGet("dcu/{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            string jsonString = "{name:Tony Stark, title:Iron Man}";
+            var data = await _exchangeRate.GetRate();
+            //return Ok(await _taskService.GetTasks("Kingslayer"));
+            return Ok(data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Event evt)
+        {
+            eventLog = "api post call --" + System.Guid.NewGuid().ToString() + " : " + System.DateTimeOffset.Now.ToString();
+            Console.WriteLine(eventLog);
+            await _publishEventService.PublishEvent(evt);
+            eventLog = "api post call ++" + System.Guid.NewGuid().ToString() + " : " + System.DateTimeOffset.Now.ToString();
+            Console.WriteLine(eventLog);
+            return Ok(System.Guid.NewGuid().ToString());
+        }
+
+    }
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ tasks.backend.initservice/Services/ExchangeRateService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using tasks.backend.initservice.Models;
+
+namespace tasks.backend.initservice.Services
+{
+    public interface IExchangeRate
+    {
+        Task<Rate> GetRate();
+    }
+    public class ExchangeRateService : IExchangeRate
+    {
+        private IConfiguration _configuration;
+        private HttpClient _httpClient;
+
+        public ExchangeRateService(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        {
+            _configuration = configuration;
+            _httpClient = httpClientFactory.CreateClient("namedClient");
+        }
+        public async Task<Rate> GetRate()
+        {
+            var currency = GetCurrency();
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri($"https://api.exchangeratesapi.io/latest?symbols={currency}"),
+                Method = HttpMethod.Get,
+            };
+            var response = await _httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<Rate>(responseContent);
+                data.Identifier = System.Guid.NewGuid().ToString();
+                return data;
+            }
+            return null;
+        }
+        private string GetCurrency()
+        {
+            var random = new Random();
+            List<string> currencies = new List<string>();
+            currencies.Add("GBP");
+            currencies.Add("INR");
+            currencies.Add("USD");
+            currencies.Add("CNY");
+            currencies.Add("CAD");
+            currencies.Add("AUD");
+            currencies.Add("MXN");
+            currencies.Add("CHF");
+            int index = random.Next(currencies.Count);
+            return currencies[index];
+        }
+    }
+}
